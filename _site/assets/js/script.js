a98e0644e9bdd1bdd5fd58ce3a53dbe21ec3cba1
daanb <daan.boertien@gmail.com>
2022-11-23 22:12:16 +0100

    building mobile menu

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -3,9 +3,11 @@
 //   }
 
 const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu")
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("is-active");  
+  navMenu.classList.toggle("open");
 });
 
 const swiper = new Swiper('.swiper', {
